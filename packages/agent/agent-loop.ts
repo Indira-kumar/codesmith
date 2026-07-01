9c2769871a702f3a981aa90ccb981749c050aea0
indira-kumar <indirakumarak@gmail.com>
2026-07-01 08:53:22 +0530

    feat(agent): make agent loop agnostic to provider

--- packages/agent/agent-loop.ts
+++ packages/agent/agent-loop.ts
@@ -7,45 +7,10 @@ export async function runAgentLoop(
   toolsMap: ToolFunctionMap,
 ) {
   while (true) {
-    let reasoning = "";
-    let content = "";
-    const toolCalls = new Map<
-      number,
-      { id: string; name: string; args: string }
-    >();
-
-    for await (const chunk of provider.stream(messages, toolsList)) {
-      const delta = chunk.choices[0].delta;
-      if (delta?.reasoning) reasoning += delta.reasoning;
-
-      if (delta.content) content += delta.content;
-
-      if (delta.tool_calls) {
-        for (const tc of delta.tool_calls) {
-          if (!toolCalls.has(tc.index))
-            toolCalls.set(tc.index, { id: "", name: "", args: "" });
-
-          const buff = toolCalls.get(tc.index)!;
-          buff.id += tc.id ?? "";
-          buff.name += tc.function?.name ?? "";
-          buff.args += tc.function?.arguments ?? "";
-        }
-      }
-    }
-
-    messages.push({
-      role: "assistant",
-      content,
-      tool_calls:
-        toolCalls.size > 0
-          ? [...toolCalls.values()].map((tc) => ({
-              id: tc.id,
-              type: "function",
-              function: { name: tc.name, arguments: tc.args },
-            }))
-          : undefined,
-    });
-
+    let { content, reasoning, toolCalls } = await provider.stream(
+      messages,
+      toolsList,
+    );
     if (toolCalls.size == 0) return content;
 
     for (const tc of toolCalls.values()) {
--- packages/ai/ollama-provider.ts
+++ packages/ai/ollama-provider.ts
@@ -1,25 +1,64 @@
 import { OpenAI } from "openai";
+import type { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 
 export class OllamaProvider {
-  private client: OpenAI;
-  private model: string;
+  client: OpenAI;
+  model: string;
 
   constructor(model: string) {
+    // super(model);
+    this.model = model;
     this.client = new OpenAI({
       apiKey: "ollama",
       baseURL: "http://localhost:11434/v1",
     });
-    this.model = model;
   }
-  async *stream(messages, tools) {
+
+  async stream(messages: ChatCompletionMessageParam[], tools: any[]) {
+    let reasoning = "";
+    let content = "";
+    const toolCalls = new Map<
+      number,
+      { id: string; name: string; args: string }
+    >();
+
     const response = await this.client.chat.completions.create({
       model: this.model,
       messages,
       tools,
       stream: true,
     });
+
     for await (const chunk of response) {
-      yield chunk;
+      const delta = chunk.choices[0].delta;
+      if (delta?.reasoning) reasoning += delta.reasoning;
+
+      if (delta.content) content += delta.content;
+
+      if (delta.tool_calls) {
+        for (const tc of delta.tool_calls) {
+          if (!toolCalls.has(tc.index))
+            toolCalls.set(tc.index, { id: "", name: "", args: "" });
+
+          const buff = toolCalls.get(tc.index)!;
+          buff.id += tc.id ?? "";
+          buff.name += tc.function?.name ?? "";
+          buff.args += tc.function?.arguments ?? "";
+        }
+      }
     }
+    messages.push({
+      role: "assistant",
+      content,
+      tool_calls:
+        toolCalls.size > 0
+          ? [...toolCalls.values()].map((tc) => ({
+              id: tc.id,
+              type: "function",
+              function: { name: tc.name, arguments: tc.args },
+            }))
+          : undefined,
+    });
+    return { content: content, reasoning: reasoning, toolCalls: toolCalls };
   }
 }
